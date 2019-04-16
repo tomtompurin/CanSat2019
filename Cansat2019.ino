7174abf7fac5f3ff0c84fe0a8c68e24a17a11246
tomtompurin <38548927+tomtompurin@users.noreply.github.com>
2019-04-16 16:07:16 +0900

    Add files via upload

---
+++ Cansat2019.ino
@@ -0,0 +1,26 @@
+/**
+ * @file cansat2019.ino
+ * @brief CanSat Project 2019
+ * @auther Tomiyoshi Yuta
+ * @date Created: 2019/04/16
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "Arduino.h"　// 便利だからいつもインクルードしてる
+#include "constant.h" // 全ての定数を管理するファイル
+#include "cansat.h" // 各センサの関数を統合した関数を管理するファイル
+
+Cansat cansat;
+
+void setup() {
+  // put your setup code here, to run once:
+  cansat.setup();
+}
+
+void loop() {
+  cansat.sensor(); // センサの値をとってくる(フィルタリング処理込み)
+  delay(50); // 動作を安定させるためにちょっと待つ
+  cansat.sequence(); // センサの値やら現在のステートやらをもとにどう制御するか
+  delay(50); // 動作を安定させるためにちょっと待つ
+}
---
+++ constant.h
@@ -0,0 +1,23 @@
+/**
+ * @file constant.h
+ * @brief CanSat Project 2019
+ * @auther Tomiyoshi Yuta
+ * @date Created: 2019/04/16
+ */
+
+ #ifndef _CONSTANT_H_
+ #define _CONSTANT_H_
+
+//ピン番号指定
+
+//シーケンス
+#define PREPARING 0 //電源on~格納検知
+#define STORED 1 //格納検知~放出検知
+#define RELEASE 2 //放出検知~飛行開始
+#define FLYING 3 //飛行開始~着陸態勢まで
+#define LANDING 4 //着陸態勢~着陸(着陸検知)（作るかどうかは未定）
+#define GOAL 5 //着陸~
+
+
+
+ #endif
